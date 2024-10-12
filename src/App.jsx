673914f51c1d39269259859731f67d3111f2f9a7
Lucas W. da Motta <lucaswantzg@gmail.com>
2024-10-12 01:14:54 -0300

    Estrutura da aplicação - Componente: Sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,25 @@
 import { Header } from "./components/Header";
 import { Post } from "./Post";
 
+import styles from "./App.module.css";
+
+import { Sidebar } from "./components/Sidebar";
 import "./global.css";
+
 
 function App() {
 	return (
 		<div>
 			<Header />
-			<Post
-				author="Lucas Motta"
-				content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad vitae eveniet sit rerum voluptate omnis, autem maiores fugiat mollitia nisi quos nobis ea doloremque tenetur voluptatibus quisquam repellendus eius modi?"
-			/>
+			<div className={styles.wrapper}>
+				<Sidebar />
+				<main>
+					<Post
+						author="Lucas Motta"
+						content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad vitae eveniet sit rerum voluptate omnis, autem maiores fugiat mollitia nisi quos nobis ea doloremque tenetur voluptatibus quisquam repellendus eius modi?"
+					/>
+				</main>
+			</div>
 		</div>
 	);
 }
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,21 @@
+import styles from "./Sidebar.module.css";
+
+export function Sidebar() {
+	return (
+		<aside className={styles.sidebar}>
+			<img
+				className={styles.cover}
+				src="https://images.unsplash.com/photo-1678055703308-42e919de802c?q=60&w=500&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+			/>
+
+			<div className={styles.profile}>
+				<strong>Lucas Motta</strong>
+				<span>Desenvolvedor Fullstack</span>
+			</div>
+
+			<footer>
+				<a href="#">Editar seu perfil</a>
+			</footer>
+		</aside>
+	);
+}
