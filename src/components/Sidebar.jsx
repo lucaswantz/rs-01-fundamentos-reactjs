04495fbf21dadf5aaaa490e2b312346e3d898450
Lucas W. da Motta <lucaswantzg@gmail.com>
2024-10-14 12:44:15 -0300

    Estrutura da aplicação - Finalizando Sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,3 +1,4 @@
+import { PencilLine } from "phosphor-react";
 import styles from "./Sidebar.module.css";
 
 export function Sidebar() {
@@ -9,12 +10,17 @@ export function Sidebar() {
 			/>
 
 			<div className={styles.profile}>
+				<img className={styles.avatar}  src="https://github.com/lucaswantz.png" />
+
 				<strong>Lucas Motta</strong>
 				<span>Desenvolvedor Fullstack</span>
 			</div>
 
 			<footer>
-				<a href="#">Editar seu perfil</a>
+				<a href="#">
+					<PencilLine size={20}/>
+					Editar seu perfil
+				</a>
 			</footer>
 		</aside>
 	);
