7afbbfcafcd4df73afa1a7c4028247bb58e277b4
Lucas W. da Motta <lucaswantzg@gmail.com>
2024-10-14 13:37:23 -0300

    Estrutura da aplicação - Formulário de comentários

--- src/App.jsx
+++ src/App.jsx
@@ -6,32 +6,31 @@ import styles from "./App.module.css";
 
 import "./global.css";
 
-
 function App() {
-	return (
-		<div>
-			<Header />
-			<div className={styles.wrapper}>
-				<Sidebar />
-				<main>
-					<Post
-						author="Lucas Motta"
-						content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad vitae eveniet sit rerum voluptate omnis, autem maiores fugiat mollitia nisi quos nobis ea doloremque tenetur voluptatibus quisquam repellendus eius modi?"
-					/>
+  return (
+    <div>
+      <Header />
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post
+            author="Lucas Motta"
+            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad vitae eveniet sit rerum voluptate omnis, autem maiores fugiat mollitia nisi quos nobis ea doloremque tenetur voluptatibus quisquam repellendus eius modi?"
+          />
 
-					<Post
-						author="Lucas Motta"
-						content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad vitae eveniet sit rerum voluptate omnis, autem maiores fugiat mollitia nisi quos nobis ea doloremque tenetur voluptatibus quisquam repellendus eius modi?"
-					/>
+          <Post
+            author="Lucas Motta"
+            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad vitae eveniet sit rerum voluptate omnis, autem maiores fugiat mollitia nisi quos nobis ea doloremque tenetur voluptatibus quisquam repellendus eius modi?"
+          />
 
-					<Post
-						author="Lucas Motta"
-						content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad vitae eveniet sit rerum voluptate omnis, autem maiores fugiat mollitia nisi quos nobis ea doloremque tenetur voluptatibus quisquam repellendus eius modi?"
-					/>
-				</main>
-			</div>
-		</div>
-	);
+          <Post
+            author="Lucas Motta"
+            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad vitae eveniet sit rerum voluptate omnis, autem maiores fugiat mollitia nisi quos nobis ea doloremque tenetur voluptatibus quisquam repellendus eius modi?"
+          />
+        </main>
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,29 +1,43 @@
 import styles from "./Post.module.css";
 
 export function Post() {
-	return (
-		<article className={styles.post}>
-			<header>
-				<div className={styles.author}>
-					<img className={styles.avatar} src="https://github.com/github.png" />
-					<div className={styles.authorInfo}>
-						<strong>Github Actions</strong>
-						<span>Desenvolvedor Fullstack</span>
-					</div>
-				</div>
-				<time title="14 de Outubro às 08:13h" dateTime="2024-10-14 08:13:30">Publicado há 1h</time>
-			</header>
+  return (
+    <article className={styles.post}>
+      <header>
+        <div className={styles.author}>
+          <img className={styles.avatar} src="https://github.com/github.png" />
+          <div className={styles.authorInfo}>
+            <strong>Github Actions</strong>
+            <span>Desenvolvedor Fullstack</span>
+          </div>
+        </div>
+        <time title="14 de Outubro às 08:13h" dateTime="2024-10-14 08:13:30">
+          Publicado há 1h
+        </time>
+      </header>
 
-			<div className={styles.content}>
-			<p>Fala galeraa 👋</p>
-			<p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
-			<p>👉{" "}<a href="#">jane.design/doctorcare</a></p>
-				<p>
-					<a href="#">#novoprojeto</a>{" "}
-					<a href="#">#nlw</a>{" "}
-					<a href="#">#rocketseat</a>{" "}
-				</p>
-			</div>
-		</article>
-	);
+      <div className={styles.content}>
+        <p>Fala galeraa 👋</p>
+        <p>
+          Acabei de subir mais um projeto no meu portifa. É um projeto que fiz
+          no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀
+        </p>
+        <p>
+          👉 <a href="#">jane.design/doctorcare</a>
+        </p>
+        <p>
+          <a href="#">#novoprojeto</a> <a href="#">#nlw</a>{" "}
+          <a href="#">#rocketseat</a>{" "}
+        </p>
+      </div>
+
+      <form className={styles.commentForm}>
+        <strong>Deixe seu feedback</strong>
+        <textarea placeholder="Deixe seu comentário" />
+        <footer>
+          <button type="submit">Comentar</button>
+        </footer>
+      </form>
+    </article>
+  );
 }
