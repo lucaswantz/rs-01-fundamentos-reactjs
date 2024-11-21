69bfabc309e11eb5c3f3dbfd9cb811d48e3f0105
Lucas W. da Motta <lucaswantzg@gmail.com>
2024-11-21 13:30:41 -0300

    Estrutura da aplicação - Componente: Avatar

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,3 +1,4 @@
+import { Avatar } from "./Avatar";
 import styles from "./Comment.module.css";
 
 import { ThumbsUp, Trash } from "phosphor-react";
@@ -5,7 +6,7 @@ import { ThumbsUp, Trash } from "phosphor-react";
 export function Comment() {
   return (
     <div className={styles.comment}>
-      <img src="https://github.com/lucaswantz.png" alt="" />
+      <Avatar hasBorder={false} src="https://github.com/lucaswantz.png" />
 
       <div className={styles.commentBox}>
         <div className={styles.commentContent}>
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,3 +1,4 @@
+import { Avatar } from "./Avatar";
 import { Comment } from "./Comment";
 import styles from "./Post.module.css";
 
@@ -6,7 +7,7 @@ export function Post() {
     <article className={styles.post}>
       <header>
         <div className={styles.author}>
-          <img className={styles.avatar} src="https://github.com/github.png" />
+          <Avatar src="https://github.com/github.png" />
           <div className={styles.authorInfo}>
             <strong>Github Actions</strong>
             <span>Desenvolvedor Fullstack</span>
