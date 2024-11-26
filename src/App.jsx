02ddcdd73e12f7cdf34d34921cd091f02f499cc4
Lucas W. da Motta <lucaswantzg@gmail.com>
2024-11-26 09:23:31 -0300

    Os motores do React - Propriedades do Post

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,49 @@ import styles from "./App.module.css";
 
 import "./global.css";
 
+// author { avatar_url: "", name: "", role: "" }
+// publishedAt: Date
+// content: String
+
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: "https://github.com/github.png",
+      name: "Github Actions",
+      role: "Desenvolvedor Fullstack",
+    },
+    content: [
+      { type: "paragraph", content: "Fala galera 👋" },
+      {
+        type: "paragraph",
+        content:
+          "Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀",
+      },
+      { type: "link", content: "jane.design/doctorcare" },
+    ],
+    publishedAt: new Date("2024-09-07 20:00:00"),
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: "https://github.com/lucaswantz.png",
+      name: "Lucas W. da Motta",
+      role: "Desenvolvedor Fullstack",
+    },
+    content: [
+      { type: "paragraph", content: "Fala galera 👋" },
+      {
+        type: "paragraph",
+        content:
+          "Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀",
+      },
+      { type: "link", content: "jane.design/doctorcare" },
+    ],
+    publishedAt: new Date("2024-10-11 20:00:00"),
+  },
+];
+
 function App() {
   return (
     <div>
@@ -13,20 +56,9 @@ function App() {
       <div className={styles.wrapper}>
         <Sidebar />
         <main>
-          <Post
-            author="Lucas Motta"
-            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad vitae eveniet sit rerum voluptate omnis, autem maiores fugiat mollitia nisi quos nobis ea doloremque tenetur voluptatibus quisquam repellendus eius modi?"
-          />
-
-          <Post
-            author="Lucas Motta"
-            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad vitae eveniet sit rerum voluptate omnis, autem maiores fugiat mollitia nisi quos nobis ea doloremque tenetur voluptatibus quisquam repellendus eius modi?"
-          />
-
-          <Post
-            author="Lucas Motta"
-            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad vitae eveniet sit rerum voluptate omnis, autem maiores fugiat mollitia nisi quos nobis ea doloremque tenetur voluptatibus quisquam repellendus eius modi?"
-          />
+          {posts.map((post) => (
+            <Post key={post.id} {...post} />
+          ))}
         </main>
       </div>
     </div>
